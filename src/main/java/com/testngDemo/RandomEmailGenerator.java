16ff6b35d6fe38ce231b684735c7c7f10e842726
Tan188 <592136197@qq.com>
2021-05-02 00:46:49 +0800

    testngdemo,@test注释

---
+++ src/main/java/com/testngDemo/RandomEmailGenerator.java
@@ -0,0 +1,11 @@
+package com.testngDemo;
+
+
+public class RandomEmailGenerator {
+
+    public String generate() {
+        return "feedback@yiibai.com";
+    }
+
+}
+
---
+++ src/main/java/com/testngDemo/TestHelloWorld.java
@@ -0,0 +1,21 @@
+package com.testngDemo;
+
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld{
+    @Test
+    public void testEmailGenerator() {
+
+        RandomEmailGenerator obj = new RandomEmailGenerator();
+        String email = obj.generate();
+
+        Assert.assertNotNull(email);
+        Assert.assertEquals(email, "feedback@yiibai.com1");
+
+    }
+
+}
+
+
